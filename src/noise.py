f6c753ecb58dfcf0f93bcf1e02137eacc9f649df
Achreko <achrekv3@gmail.com>
2022-12-29 16:25:26 +0100

    Decent map generation, lacking squarebump

--- src/noise.py
+++ src/noise.py
@@ -6,8 +6,10 @@ import random
 from land_enum import Land
 
 def generate():
-    noise_gen_height = OpenSimplex(seed=random.randint(0,10**6))
-    noise_gen_moist = OpenSimplex(seed=random.randint(-100000,0))
+    seed_h = random.randint(0,10**6)
+    seed_m = random.randint(-100000,0)
+    noise_gen_height = OpenSimplex(seed=seed_h)
+    noise_gen_moist = OpenSimplex(seed=seed_m)
 
     width = 256
     height = 256
@@ -17,28 +19,26 @@ def generate():
         for y in range(height):
             nx = x/width - 0.5
             ny = y/height - 0.5
+
             e1,e2,e3,e4,e5,e6 = 1, 0.5, 0.25, 0.12, 0.06, 0.03
-            exp = 1
-            elev =  e1 * noise_gen_height.noise2(1*nx,1*ny) /2.0 + 0.5 + \
-                    e2 * noise_gen_height.noise2(2*nx,2*ny) /2.0 + 0.5+ \
-                    e3 * noise_gen_height.noise2(4*nx,4*ny) /2.0 + 0.5+ \
-                    e4 * noise_gen_height.noise2(8*nx,8*ny) /2.0 + 0.5+ \
-                    e5 * noise_gen_height.noise2(16*nx,16*ny) /2.0 + 0.5+ \
-                    e6 * noise_gen_height.noise2(32*nx,32*ny)/2.0 + 0.5
-            elev = elev/sum((e1,e2,e3,e4,e5,e6))
+            exp = 4
+            elev =  e1 * (noise_gen_height.noise2(1*nx,1*ny) /2.0 + 0.5) + \
+                    e2 * (noise_gen_height.noise2(2*nx,2*ny) /2.0 + 0.5)+ \
+                    e3 * (noise_gen_height.noise2(4*nx,4*ny) /2.0 + 0.5)+ \
+                    e4 * (noise_gen_height.noise2(8*nx,8*ny) /2.0 + 0.5)+ \
+                    e5 * (noise_gen_height.noise2(16*nx,16*ny) /2.0 + 0.5)+ \
+                    e6 * (noise_gen_height.noise2(32*nx,32*ny)/2.0 + 0.5)
 
             m1,m2,m3,m4,m5,m6 = 1, 0.5, 0.25, 0.12, 0.06, 0.03
-            moist = m1 * noise_gen_moist.noise2(1*nx,1*ny) /2.0 + 0.5+ \
-                    m2 * noise_gen_moist.noise2(2*nx,2*ny) /2.0 + 0.5+ \
-                    m3 * noise_gen_moist.noise2(4*nx,4*ny) /2.0 + 0.5+ \
-                    m4 * noise_gen_moist.noise2(8*nx,8*ny) /2.0 + 0.5+ \
-                    m5 * noise_gen_moist.noise2(16*nx,16*ny) /2.0 + 0.5+ \
-                    m6 * noise_gen_moist.noise2(32*nx,32*ny)/2.0 + 0.5
-            heights[y][x] = biome(pow(elev,exp), moist)
-
-    # Create an image from the heightmap array using the PIL library
-    img = Image.fromarray(heights, 'I')
-
+            moist = m1 * (noise_gen_moist.noise2(1*nx,1*ny) /2.0 + 0.5)+ \
+                    m2 * (noise_gen_moist.noise2(2*nx,2*ny) /2.0 + 0.5)+ \
+                    m3 * (noise_gen_moist.noise2(4*nx,4*ny) /2.0 + 0.5)+ \
+                    m4 * (noise_gen_moist.noise2(8*nx,8*ny) /2.0 + 0.5)+ \
+                    m5 * (noise_gen_moist.noise2(16*nx,16*ny) /2.0 + 0.5)+ \
+                    m6 * (noise_gen_moist.noise2(32*nx,32*ny)/2.0 + 0.5)
+            heights[y][x] = biome(pow(elev *1.15,exp), moist)
+    
+    img = Image.fromarray(heights, 'RGB')
     img.save(f'results/heightmap{noise_gen_height.noise2(1/e1*nx,1/e1*ny)}.png')
 
 
