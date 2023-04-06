b6bd1e89ccdccfd9bcf97fa335a0e445942a707e
Achreko <achrekv3@gmail.com>
2023-04-06 16:13:28 +0200

    Modified heights array type

--- src/noise.py
+++ src/noise.py
@@ -5,6 +5,7 @@ import random
 from land_enum import Land
 from image_functions import save_img
 from gradients import *
+from rivers import *
 
 
 def generate():
@@ -22,9 +23,9 @@ def generate():
     exp = 2
     heights =  np.zeros((HEIGHT,WIDTH,3), dtype=np.uint8)
     # gradients = [circular_gradient((HEIGHT,WIDTH),50, 2),circular_gradient((HEIGHT,WIDTH),50, 2.5),circular_gradient((HEIGHT,WIDTH),50, 1.5)]
-    grad = circular_gradient((HEIGHT,WIDTH),150, 2)
+    # grad = circular_gradient((HEIGHT,WIDTH),150, 2)
 
-    e_map, m_map, t_map = np.zeros((HEIGHT,WIDTH), dtype=np.uint8), np.zeros((HEIGHT,WIDTH), dtype=np.uint8), np.zeros((HEIGHT,WIDTH), dtype=np.uint8)
+    e_map, m_map, t_map = np.empty((HEIGHT,WIDTH), dtype=np.uint8), np.empty((HEIGHT,WIDTH), dtype=np.uint8), np.empty((HEIGHT,WIDTH), dtype=np.uint8)
 
     for y in range(HEIGHT):
         for x in range(WIDTH):
@@ -33,15 +34,15 @@ def generate():
 
             # elevation
             e = [1, 0.5, 0.25, 0.13, 0.06, 0.03]
-            # elev = (noise_gen_height.noise2(x / FEATURE_SIZE,y / FEATURE_SIZE) /2.0 + 0.5) #for archipelago
-            # elev = (ridgenoise(noise_gen_height.noise2(x / FEATURE_SIZE,y / FEATURE_SIZE) /2.0 + 0.5)) # for mountains
-            elev =  e[0] * (noise_gen_height.noise2(nx / FEATURE_SIZE,ny / FEATURE_SIZE) /2.0 + 0.5) + \
-                    e[1] * (noise_gen_height.noise2(2*nx + 2.137 / FEATURE_SIZE,(2*ny + 3.75) / FEATURE_SIZE) /2.0 + 0.5) + \
-                    e[2] * (noise_gen_height.noise2(4*nx + 690 / FEATURE_SIZE,(4*ny + 690) / FEATURE_SIZE) /2.0 + 0.5) + \
-                    e[3] * (noise_gen_height.noise2((8*nx + 4200) / FEATURE_SIZE,(8*ny +4200) / FEATURE_SIZE) /2.0 + 0.5)+ \
-                    e[4] * (noise_gen_height.noise2((16*nx + 1800) / FEATURE_SIZE,(16*ny + 1800) / FEATURE_SIZE) /2.0 + 0.5)+ \
-                    e[5] * (noise_gen_height.noise2((32*nx + 2137) / FEATURE_SIZE,(32*ny + 2137) / FEATURE_SIZE)/2.0 + 0.5)
-            elev = elev/sum(e)
+            elev = (noise_gen_height.noise2(x / FEATURE_SIZE,y / FEATURE_SIZE) /2.0 + 0.5) #for archipelago
+            # elev = (ridge_noise(noise_gen_height.noise2(x / FEATURE_SIZE,y / FEATURE_SIZE) /2.0 + 0.5)) # for mountains
+            # elev =  e[0] * (noise_gen_height.noise2(x / FEATURE_SIZE,ny / FEATURE_SIZE) /2.0 + 0.5) + \
+            #         e[1] * (noise_gen_height.noise2((2*x + 2.137) / FEATURE_SIZE,(2*y + 3.75) / FEATURE_SIZE) /2.0 + 0.5) + \
+            #         e[2] * (noise_gen_height.noise2((4*x + 690) / FEATURE_SIZE,(4*y + 690) / FEATURE_SIZE) /2.0 + 0.5) + \
+            #         e[3] * (noise_gen_height.noise2((8*x + 4200) / FEATURE_SIZE,(8*y +4200) / FEATURE_SIZE) /2.0 + 0.5)+ \
+            #         e[4] * (noise_gen_height.noise2((16*x + 1800) / FEATURE_SIZE,(16*y + 1800) / FEATURE_SIZE) /2.0 + 0.5)+ \
+            #         e[5] * (noise_gen_height.noise2((32*x + 2137) / FEATURE_SIZE,(32*y + 2137) / FEATURE_SIZE)/2.0 + 0.5)
+            # elev = elev/sum(e)
             #moisture
             m = [1, 0.5, 0.25, 0.13, 0.06, 0.03]
             moist = m[0] * (noise_gen_moist.noise2(1*nx,1*ny) /2.0 + 0.5) + \
@@ -64,30 +65,27 @@ def generate():
             t_map[y][x] = temperature*255
 
             # square bump for island
-            nx = 2*x/WIDTH - 1
-            ny = 2*y/HEIGHT - 1
+            # nx = 2*x/WIDTH - 1
+            # ny = 2*y/HEIGHT - 1
 
-            d = 1 - (1-nx**2) * (1-ny**2)
-            elev = (elev + (1-d))/2
+            # d = 1 - (1-nx**2) * (1-ny**2)
+            # elev = (elev + (1-d))/2
 
             # mountains
 
 
-            # heights[y][x] = biome(pow(elev *fudge_factor,exp), moist, temperature)
-            #archipelago
             e_map[y][x] = elev*255
             
             fudge_factor = 1.15
-
-            # heights[y][x] = biome(pow(elev *fudge_factor * grad[y][x]/255,exp), moist, temperature)
-            heights[y][x] = biome(pow(elev *fudge_factor,exp), moist, temperature)
-
+            # heights[y][x] = biome(pow(elev *fudge_factor,exp), moist, temperature)
+            heights[y][x] = biome(pow(elev *fudge_factor * grad[y][x]/255,exp), moist, temperature) #for archipelago
             
     # save_img(grad, "grad", "L")
     save_img(heights, "landmap", 'RGB')
     save_img(e_map,"elev",'L')
     save_img(m_map,"moist",'L')
     save_img(t_map,"temperature",'L')
+    # river_generation(1, heights,256)
     
 
 
