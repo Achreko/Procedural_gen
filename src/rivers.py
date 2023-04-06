ab2a38843191ad74ceeabdf2aa9c4adf42f04ced
Achreko <achrekv3@gmail.com>
2023-04-06 22:15:31 +0200

    Implemented voronoi cells

--- src/rivers.py
+++ src/rivers.py
@@ -1,14 +1,23 @@
 import random
 import numpy as np
-from scipy.spatial import Voronoi, voronoi_plot_2d
-import matplotlib.pyplot as plt
+from scipy.spatial import Voronoi
+from PIL import Image, ImageDraw
 
-
-def river_generation(river_number: int, map: np.ndarray, point_number: int) -> np.ndarray:
-    shp = map.shape
-    points = [[random.randrange(shp[0]) - random.randrange(shp[0]), random.randrange(shp[0]) - random.randrange(shp[0])]
+def river_generation(river_number: int, map: np.ndarray, shp: tuple, point_number: int) -> np.ndarray:
+    points = [[random.randrange(shp[0]), random.randrange(shp[0])]
      for xd in range(point_number)]
+    points.append((-shp[0]*3, -shp[1]*3))
+    points.append((-shp[0]*3, shp[1]*4))
+    points.append((shp[0]*4, -shp[1]*3))
+    points.append((shp[0]*4, shp[1]*4))
+    
+    img = Image.new("L", shp, color=255)
+    draw = ImageDraw.Draw(img)
     vor = Voronoi(points)
-    fig = voronoi_plot_2d(vor)
-    plt.show()
+    vor_vertices = vor.vertices
+    for region in vor.regions[1:]:
+        if -1 not in region:
+            polygon = [tuple(vor_vertices[p]) for p in region]
+            draw.polygon(polygon, outline='black')
+    img.save("results/vor.png")
     return map
