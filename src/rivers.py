46239df2813355d41b6a49a59f8f9484aa1312f5
Achreko <achrekv3@gmail.com>
2023-04-06 16:13:44 +0200

    Voronoi beginnings

--- src/rivers.py
+++ src/rivers.py
@@ -1,6 +1,14 @@
-import pandas as pd
 import random
 import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import matplotlib.pyplot as plt
 
-#𝑦=𝛽+θnXn, where x is the parameters(can go from 1 to n), 𝛽 is the bias and θ is the weight
 
+def river_generation(river_number: int, map: np.ndarray, point_number: int) -> np.ndarray:
+    shp = map.shape
+    points = [[random.randrange(shp[0]) - random.randrange(shp[0]), random.randrange(shp[0]) - random.randrange(shp[0])]
+     for xd in range(point_number)]
+    vor = Voronoi(points)
+    fig = voronoi_plot_2d(vor)
+    plt.show()
+    return map
